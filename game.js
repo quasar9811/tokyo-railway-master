2f4b2c16481823697b0148c9801688d8a3a2ea4b
quasar9811 <donghyun981102@gmail.com>
2026-06-18 11:01:00 +0900

    Update game.js

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const GAME_TIME = 120;
+const GAME_TIME = 100;
 
 const lines = [
   { name: "山手線", company: "JR", level: "easy", stations: ["東京","神田","秋葉原","御徒町","上野","鶯谷","日暮里","西日暮里","田端","駒込","巣鴨","大塚","池袋","目白","高田馬場","新大久保","新宿","代々木","原宿","渋谷","恵比寿","目黒","五反田","大崎","品川","高輪ゲートウェイ","田町","浜松町","新橋","有楽町"] },
